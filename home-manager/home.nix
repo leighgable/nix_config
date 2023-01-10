660d34c61b4cd761071b4db907737a38f3513be9
Leigh Gable <leighgable@gmail.com>
2023-01-10 18:42:49 +0100

    trying a template from misterio77

---
+++ home-manager/home.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "leigh";
+  home.homeDirectory = "/home/leigh";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  # programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    tmux
+    # gh
+    google-chrome
+    git
+    # emacs
+    pass      # password manager
+    feh       # image viewer
+    moc       # music on the command line
+    zathura   # ebooks viewer ?
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Leigh Gable";
+    userEmail = "leighgable@gmail.com";
+    aliases = {
+      ci = "commit";
+      s = "status";
+      co = "checkout";
+    };
+  };
+
+  services.gpg-agent {
+    enable = true;
+    enableSshSupport = true;
+  };
+  
+ # programs.gh = {
+ #   enable = true;
+ # };
+
+#  programs.emacs = {
+#    enable = true;
+#    extraPackages = epkgs: [
+#      epkgs.nix-mode
+#      epkgs.magit
+#    ];
+#  };
+}
