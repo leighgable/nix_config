cb97123c2f4f166d628176ee22447014b640ae85
Leigh Gable <leighgable@gmail.com>
2023-01-15 00:01:43 +0100

    add helix overlay

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Your new nix config";
+  description = "Here goes nothing";
 
   inputs = {
     # Nixpkgs
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -19,37 +19,8 @@
       #        patches = [ ./change-hello-to-hi.patch ];
       #     });
       #  })
-      (final: prev: {
-          helix = final.pkgs.helix.override {
-            extraWrapperArgs = [
-              "--set-default"
-              "RUST_SRC_PATH"
-              "${pkgs.rustPlatform.rustcSrc}/library"
-            ];
-            languageServers = with pkgs; [
-              nodePackages.bash-language-server
-              shellcheck
-              yaml-language-server
-              cmake-language-server
-              taplo-lsp
-              rnix-lsp
-              alejandra
-              python3Packages.python-lsp-server
-              clang-tools
-              rust-analyzer
-              haskellPackages.haskell-language-server
-              # inputs.nickel.packages.default
-              # inputs.nil.packages.default
-              gopls
-              julia
-              shfmt
-              go
-              black
-            ];
-          }; 
-          };
-          )
-
+      ( import ./helix-overlay.nix )
+      ];
       config = {
         allowUnfree = true;
         allowUnfreePredicate = (_: true);
