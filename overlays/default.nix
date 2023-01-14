1f896fe67d77af345fa2bc21be2a98ee576c02c4
Leigh Gable <leighgable@gmail.com>
2023-01-14 01:42:43 +0100

    helix override attempt

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,3 @@
-{ pkgs ? import <nixpkgs> { } }: 
 # This file defines overlays
 {
   # This one brings our custom packages from the 'pkgs' directory
@@ -10,32 +9,5 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
-    helix = prev.pkgs.helix.override {
-      extraWrapperArgs = [
-        "--set-default"
-        "RUST_SRC_PATH"
-        "${pkgs.rustPlatform.rustcSrc}/library"
-      ];
-      languageServers = with pkgs; [
-        nodePackages.bash-language-server
-        shellcheck
-        yaml-language-server
-        cmake-language-server
-        taplo-lsp
-        rnix-lsp
-        alejandra
-        python3Packages.python-lsp-server
-        clang-tools
-        rust-analyzer
-        haskellPackages.haskell-language-server
-        # inputs.nickel.packages.default
-        # inputs.nil.packages.default
-        gopls
-        julia
-        shfmt
-        go
-        black
-      ];
-    }; 
   };
 }
