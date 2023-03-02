0243b2f8d73598c7f5f86e3d6f82cb41f3d29022
Leigh Gable <leighgable@gmail.com>
2023-03-02 18:47:14 +0100

    get colors back

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -196,37 +196,37 @@
   
   programs.bash.enable = true;
   
-  # programs.dircolors.enable = true;
-  # programs.dircolors.enableBashIntegration = true;
+  programs.dircolors.enable = true;
+  programs.dircolors.enableBashIntegration = true;
   programs.starship.enableBashIntegration = true;
-  # programs.dircolors.extraConfig = ''
-  #   TERM xterm-256color
-  # '';
-  # programs.dircolors.settings = {
-  #   ".iso" = "01;31"; # .iso files bold red like .zip and other archives
-  #   ".gpg" = "01;33"; # .gpg files bold yellow
-  #   # Images to non-bold magenta instead of bold magenta like videos
-  #   ".bmp"   = "00;35";
-  #   ".gif"   = "00;35";
-  #   ".jpeg"  = "00;35";
-  #   ".jpg"   = "00;35";
-  #   ".mjpeg" = "00;35";
-  #   ".mjpg"  = "00;35";
-  #   ".mng"   = "00;35";
-  #   ".pbm"   = "00;35";
-  #   ".pcx"   = "00;35";
-  #   ".pgm"   = "00;35";
-  #   ".png"   = "00;35";
-  #   ".ppm"   = "00;35";
-  #   ".svg"   = "00;35";
-  #   ".svgz"  = "00;35";
-  #   ".tga"   = "00;35";
-  #   ".tif"   = "00;35";
-  #   ".tiff"  = "00;35";
-  #   ".webp"  = "00;35";
-  #   ".xbm"   = "00;35";
-  #   ".xpm"   = "00;35";
-  # };
+  programs.dircolors.extraConfig = ''
+    TERM xterm-256color
+  '';
+  programs.dircolors.settings = {
+    ".iso" = "01;31"; # .iso files bold red like .zip and other archives
+    ".gpg" = "01;33"; # .gpg files bold yellow
+    # Images to non-bold magenta instead of bold magenta like videos
+    ".bmp"   = "00;35";
+    ".gif"   = "00;35";
+    ".jpeg"  = "00;35";
+    ".jpg"   = "00;35";
+    ".mjpeg" = "00;35";
+    ".mjpg"  = "00;35";
+    ".mng"   = "00;35";
+    ".pbm"   = "00;35";
+    ".pcx"   = "00;35";
+    ".pgm"   = "00;35";
+    ".png"   = "00;35";
+    ".ppm"   = "00;35";
+    ".svg"   = "00;35";
+    ".svgz"  = "00;35";
+    ".tga"   = "00;35";
+    ".tif"   = "00;35";
+    ".tiff"  = "00;35";
+    ".webp"  = "00;35";
+    ".xbm"   = "00;35";
+    ".xpm"   = "00;35";
+  };
   
   programs.starship.enable = true;
   programs.starship.settings = {
