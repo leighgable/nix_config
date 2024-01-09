3b84aa69361243aa4cc01411216d646b1abf3b8e
Leigh Gable <leighgable@gmail.com>
2024-01-09 20:03:07 +0100

    first

---
+++ home-manager/home.nix
@@ -0,0 +1,101 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # (import ./programs/helix { inherit pkgs lib inputs ; })
+    # (import ./programs/helix/languages.nix { inherit pkgs lib; })
+    # (import ./programs/zellij { inherit pkgs config; })
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  # TODO: Set your username
+  home = {
+    username = "leigh";
+    homeDirectory = "/home/leigh";
+  };
+
+  # Add stuff for your user as you see fit:
+  # programs.neovim.enable = true;
+  # home.packages = with pkgs; [ steam ];
+
+  home.packages = with pkgs; [
+    google-chrome 
+    git
+    nix-direnv
+    gh
+    glow
+    pass      # password manager
+    feh       # image viewer
+    moc       # music on the command line
+    tree
+    sioyek
+    helix
+    zellij
+    ripgrep
+    broot     # filesystem browser
+    exa       # ls type thing
+    bat       # cat with syntax highlighting
+    du-dust   # disk usage
+    fd        # simpler find
+    procs     # ps in rust
+    tealdeer  # tldr --update
+    bottom    # top but rust
+    skim      # rusty grep
+    zoxide    # directory jumper
+    xsel
+    unzip
+    curl
+    wget
+  ];
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
